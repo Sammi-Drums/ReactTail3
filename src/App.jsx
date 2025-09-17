6ba26f92ea9e5f1103a2ee46869a36dec148ee6c
Akia Samuel Chah <80036169+Sammi-Drums@users.noreply.github.com>
2025-09-17 12:09:31 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Tailwind Works 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
