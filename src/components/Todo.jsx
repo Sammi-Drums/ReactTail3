937218a8d76d28f55b8f378532aab5ba0b143fe4
Akia Samuel Chah <80036169+Sammi-Drums@users.noreply.github.com>
2025-09-19 22:08:16 +0100

    Delete feature added and implemented with javaScript

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
 
 const Todo = () => {
+    const [todoList, setTodoList] = useState([]);
+    const inputRef = useRef();
+    const add = () =>{
+        const inputText = inputRef.current.value.trim();
+        if(inputText ===""){
+            return null;
+        }
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false,
+        }
+        setTodoList ((prev)=>[...prev, newTodo]);
+        inputRef.current.value = "";
+        
+    }
+    const deleteTodo = (id) =>{
+        setTodoList((prvTodos)=> {
+            return prvTodos.filter((todo)=> todo.id !== id);
+        });
+
+    }
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
          { /* -------------------------- Title section ----------------------------------- */}
         <div className='flex items-center mt-7 gap-2'>
             <img className='w-8' src={todo_icon} alt="" />
-            <h1 className='font-semibold text-3xl'>To-Do List</h1>
+            <h1 className='font-semibold text-3xl'>To-Do List</h1> 
         </div>
         { /* -------------------------- Input Box ----------------------------------- */}
         <div className='flex item-center my-7 bg-gray-200 rounded-full'>
-            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your Task' />
-            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-large font-medium cursor-ponter'>ADD +</button>
+            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 
+            placeholder:text-slate-600' type="text" placeholder='Add your Task' />
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white 
+            text-large font-medium cursor-ponter'>ADD +</button>
         </div>
         { /* -------------------------- Todo List section ----------------------------------- */}
         <div>
-            
+            {todoList.map((item, index)=>{
+                return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} 
+                deleteTodo={deleteTodo} />
+            })}
         </div>
     </div>
     
